b21c9f189219fb9b100dfc6088c3d3cc04ac8a80
SauravRupiksha <developer@rupiksha.com>
2026-03-11 14:15:00 +0530

    Add files via upload

---
+++ FingPay/FingPay/src/main/java/com/company/fingpay/FingPay/FingPayApplication.java
@@ -0,0 +1,13 @@
+package com.company.fingpay.FingPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingPayApplication.class, args);
+	}
+
+}
---
+++ FingPay/FingPay/src/test/java/com/company/fingpay/FingPay/FingPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.fingpay.FingPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
